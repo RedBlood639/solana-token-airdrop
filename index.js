cdc301f3d6e02877225b162ee17c51e8360eec6c
Redblood639 <albinorabbithunter@gmail.com>
2022-07-22 05:23:04 +0900

    fix : express pharser

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
 import { config } from "dotenv";
 import airdrop from "./src/apis/airdrop.js";
-
+const PORT = process.env.PORT || 3000;
 config();
 
 const app = express();
-
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/api/airdrop", airdrop);
-
-const PORT = process.env.PORT || 3000;
-
+app.use(cors("*"));
+app.use(express.json());
+// controllers
+app.use("/", airdrop);
+// server deploy
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });

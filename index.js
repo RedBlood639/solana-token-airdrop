a6ba7557fe269a2673cdaabcbabc5e2bae289e02
Redblood639 <albinorabbithunter@gmail.com>
2022-07-22 03:51:16 +0900

    fix : cors error

--- index.js
+++ index.js
@@ -8,11 +8,23 @@ config();
 
 const app = express();
 
-var corsOptions = {
-  origin: "*",
-};
+const allowedOrigins = ["https://spl-airdrop.vercel.app"];
 
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 app.use(bodyParser.json());
 
